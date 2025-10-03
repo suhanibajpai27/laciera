745cfa1849d479fc03d3780caec6ea6155fbceba
Suhani Bajpai <bsuhani702@gmail.com>
2025-10-03 12:16:51 +0530

    products page resp

--- src/components/products/Catalog.jsx
+++ src/components/products/Catalog.jsx
@@ -4,8 +4,8 @@ import { products } from "../../data/Products";
 
 const Catalog = () => {
   return (
-    <section className="w-full py-20 px-6 lg:px-20">
-      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+    <section className="w-full py-10 md:py-20 px-6 lg:px-20">
+      <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-7xl mx-auto">
         {products.map((product) => (
           <Link
             key={product.id}
@@ -23,10 +23,10 @@ const Catalog = () => {
             <div className="absolute bottom-10 left-5 right-5 bg-white py-3 flex items-center">
               <div className="w-1 bg-green-600 h-12 mr-3"></div>
               <div className="text-left">
-                <p className="text-[16px] text-primary-2 font-medium">
+                <p className="text-[14px] lg:text-[16px] text-primary-2 font-medium">
                   {product.category}
                 </p>
-                <h3 className="text-[20px] font-bold text-text-1">
+                <h3 className="text-[16px] md:text-[16px] lg:text-[20px] font-bold text-text-1">
                   {product.title}
                 </h3>
               </div>
